2c173a067565e6922631bbd1584e089a602276b6
debansu12 <debansudas6017@gmail.com>
2025-11-04 12:59:43 +0530

    Update HomeTileData.js

--- src/components/HomeTileData.js
+++ src/components/HomeTileData.js
@@ -27,9 +27,7 @@ const abc = [
       "https://images-na.ssl-images-amazon.com/images/M/MV5BMjEyOTYyMzUxNl5BMl5BanBnXkFtZTcwNTg0MTUzNA@@._V1_SX1500_CR0,0,1500,999_AL_.jpg",
       "https://images-na.ssl-images-amazon.com/images/M/MV5BNzM2MDk3MTcyMV5BMl5BanBnXkFtZTcwNjg0MTUzNA@@._V1_SX1777_CR0,0,1777,999_AL_.jpg",
       "https://images-na.ssl-images-amazon.com/images/M/MV5BMTY2ODQ3NjMyMl5BMl5BanBnXkFtZTcwODg0MTUzNA@@._V1_SX1777_CR0,0,1777,999_AL_.jpg",
-      "https://images-na.ssl-images-amazon.com/images/M/MV5BMTMxOTEwNDcxN15BMl5BanBnXkFtZTcwOTg0MTUzNA@@._V1_SX1777_CR0,0,1777,999_AL_.jpg",
-      "https://images-na.ssl-images-amazon.com/images/M/MV5BMTYxMDg1Nzk1MV5BMl5BanBnXkFtZTcwMDk0MTUzNA@@._V1_SX1500_CR0,0,1500,999_AL_.jpg",
-    ],
+      "https://th.bing.com/th/id/OIP.Js-64ytCEYEmRkBcX7O-4wHaKz?w=185&h=269&c=7&r=0&o=7&pid=1.7&rm=3",   
   },
   {
     id: 1,
@@ -267,3 +265,4 @@ const abc = [
 ];
 
 export default abc;
+
